326bb32c74a92712c57ae67a2066191ffa526ff7
liupu <178853518@qq.com>
2019-04-21 23:54:28 +0800

    update code

--- src/reducer/index.js
+++ src/reducer/index.js
@@ -1,12 +1,13 @@
 export default (state = { r: 20, g: 220, b: 220 }, action) => {
     let { type } = action;
     switch (type) {
-        case 'R':
-            return { ...state, r: state.r + action.v };
-        case 'G':
-            return { ...state, g: state.g + action.v };
-        case 'B':
-            return { ...state, b: state.b + action.v };
+        case 'CHANGE':
+            console.log([action.color])
+            return { ...state, [action.color]:action.v+state.r};
+        // case 'G':
+        //     return { ...state, g: state.g + action.v };
+        // case 'B':
+        //     return { ...state, b: state.b + action.v };
         default:
             return state;
     }
