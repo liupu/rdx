811f239938c750f34b830b27a801fba8f33b599c
liupu <178853518@qq.com>
2019-04-26 23:45:11 +0800

    update code for justify function

--- src/components/item/index.js
+++ src/components/item/index.js
@@ -4,8 +4,6 @@ import './index.css';
 export default class Item extends Component {
     constructor(props){
         super(props)
-        console.log("props::::",props)
-       // console.log(this.props.item)
     }
     handleCheck(id){
         this.props.actions.checkFun(id);
@@ -13,16 +11,15 @@ export default class Item extends Component {
     handleRemove(id){
         this.props.actions.removeFun(id);
     }
-
-    
     render() {
+        const { item } = this.props;
          return (
             <div>
-                <input type='checkbox' checked={this.props.item.done} onChange={this.handleCheck.bind(this, this.props.item.id)} />
+                <input type='checkbox' checked={item.done} onChange={this.handleCheck.bind(this, item.id)} />
                 {' '}
-                <span>{this.props.item.text}</span>
+                <span>{item.text}</span>
                 {' '}
-                <input type='button' value='delete' onClick={this.handleRemove.bind(this, this.props.item.id)} />
+                <input type='button' value='delete' onClick={this.handleRemove.bind(this, item.id)} />
             </div>
         )
     }
